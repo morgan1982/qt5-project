43b543d073b4ad31aff1b85c64abcaeef180fb3d
entropy <alexandrisanas@gmail.com>
2018-01-16 00:06:29 +0200

    add the icon

--- main_window.py
+++ main_window.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtGui, QtCore
 
 
 def window():
@@ -8,6 +8,12 @@ def window():
     l1 = QtWidgets.QLabel(w)
     l1.setText('Option 1')
     l1.move(300, 100)
+    l2 = QtWidgets.QLabel(w)
+    l2.setText('keychain')
+    pixmap = QtGui.QPixmap('./images/key.png')
+    pixmap_scaled = pixmap.scaled(128, 128, QtCore.Qt.KeepAspectRatio)
+    l2.setPixmap(pixmap_scaled)
+    l2.move(600, 20)
     w.show()
     w.setWindowTitle('pass app')
     w.setGeometry(1600, 200, 800, 600)
